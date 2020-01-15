5058c18b898e4cbb591ec9a2a9ee90fbb66def60
Lawrencesbai <baisong@shanghaitech.edu.cn>
2020-01-15 09:56:47 -0800

    all

---
+++ proj-rt-files/simplemath.h
@@ -0,0 +1,18 @@
+# ifndef _SIMPLEMATH_H_
+# define _SIMPLEMATH_H_
+
+#include
+
+struct sol{
+    bool solvable;
+    double solution[2];
+};
+
+class simplemath(){
+public:
+    sol QuadEq()
+
+
+}
+
+# endif
